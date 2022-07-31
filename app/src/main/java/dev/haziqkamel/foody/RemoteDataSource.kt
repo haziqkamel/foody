c563b286a194da60c9e146e98b7fca579e7ed5ec
Haziq Kamel <haziqkamel@outlook.com>
2022-07-31 21:18:29 +0800

    Inject RemoteDataSource class

--- app/src/main/java/dev/haziqkamel/foody/RemoteDataSource.kt
+++ app/src/main/java/dev/haziqkamel/foody/RemoteDataSource.kt
@@ -1,5 +1,13 @@
 package dev.haziqkamel.foody
 
-class RemoteDataSource() {
+import dev.haziqkamel.foody.models.foodRecipe.FoodRecipe
+import retrofit2.Response
+import javax.inject.Inject
 
+class RemoteDataSource @Inject constructor(
+    private val foodRecipesApi: FoodRecipesApi,
+) {
+    suspend fun getRecipes(queries: Map<String, String>): Response<FoodRecipe> {
+        return foodRecipesApi.getRecipes(queries)
+    }
 }
