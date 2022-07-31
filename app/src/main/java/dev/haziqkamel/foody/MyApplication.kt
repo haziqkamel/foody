bf0ced3339236fc7a62b9f2d358b1d65dcfd85cd
Haziq Kamel <haziqkamel@outlook.com>
2022-07-31 20:56:57 +0800

    Create MyApplication class

---
+++ app/src/main/java/dev/haziqkamel/foody/MyApplication.kt
@@ -0,0 +1,9 @@
+package dev.haziqkamel.foody
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication: Application() {
+
+}
---
+++ app/src/main/java/dev/haziqkamel/foody/RemoteDataSource.kt
@@ -0,0 +1,5 @@
+package dev.haziqkamel.foody
+
+class RemoteDataSource() {
+
+}
