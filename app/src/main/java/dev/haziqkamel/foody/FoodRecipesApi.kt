0d9cc323cea72c668e85b444a593cdb10c7ea40e
Haziq Kamel <haziqkamel@outlook.com>
2022-07-31 18:08:41 +0800

    Create RecipesApi Interface

---
+++ app/src/main/java/dev/haziqkamel/foody/FoodRecipesApi.kt
@@ -0,0 +1,12 @@
+package dev.haziqkamel.foody
+
+import dev.haziqkamel.foody.models.foodRecipe.FoodRecipe
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.QueryMap
+
+interface FoodRecipesApi {
+
+    @GET("/recipes/complexSearch")
+    suspend fun getRecipes(@QueryMap queries: Map<String, String>): Response<FoodRecipe>
+}
