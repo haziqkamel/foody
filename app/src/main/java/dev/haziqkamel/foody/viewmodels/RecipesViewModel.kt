33b484e7b8f599550c5045864e51469813865102
Haziq Kamel <haziqkamel@outlook.com>
2022-08-08 16:56:11 +0800

    Create and Inject RecipesViewModel

--- app/src/main/java/dev/haziqkamel/foody/viewmodels/RecipesViewModel.kt
+++ app/src/main/java/dev/haziqkamel/foody/viewmodels/RecipesViewModel.kt
@@ -3,6 +3,9 @@ package dev.haziqkamel.foody.viewmodels
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import dev.haziqkamel.foody.util.Constant.Companion.API_KEY
+import dev.haziqkamel.foody.util.Constant.Companion.DEFAULT_DIET_TYPE
+import dev.haziqkamel.foody.util.Constant.Companion.DEFAULT_MEAL_TYPE
+import dev.haziqkamel.foody.util.Constant.Companion.DEFAULT_RECIPES_NUMBER
 import dev.haziqkamel.foody.util.Constant.Companion.QUERY_ADD_INFORMATION
 import dev.haziqkamel.foody.util.Constant.Companion.QUERY_API_KEY
 import dev.haziqkamel.foody.util.Constant.Companion.QUERY_DIET
@@ -15,10 +18,10 @@ class RecipesViewModel(application: Application) : AndroidViewModel(application)
     fun applyQueries(): HashMap<String, String> {
         val queries: HashMap<String, String> = HashMap()
 
-        queries[QUERY_NUMBER] = "50"
+        queries[QUERY_NUMBER] = DEFAULT_RECIPES_NUMBER
         queries[QUERY_API_KEY] = API_KEY
-        queries[QUERY_TYPE] = "snack"
-        queries[QUERY_DIET] = "vegan"
+        queries[QUERY_TYPE] = DEFAULT_MEAL_TYPE
+        queries[QUERY_DIET] = DEFAULT_DIET_TYPE
         queries[QUERY_ADD_INFORMATION] = "true"
         queries[QUERY_FILL_INGREDIENTS] = "true"
 
