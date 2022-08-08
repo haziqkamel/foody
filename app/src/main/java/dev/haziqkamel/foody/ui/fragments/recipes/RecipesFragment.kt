2fce2a7a04ee9187346c5ac0da5cdc7129ccd1aa
Haziq Kamel <haziqkamel@outlook.com>
2022-08-08 16:00:30 +0800

    Navigate to Recipes Bottom Sheet

--- app/src/main/java/dev/haziqkamel/foody/ui/fragments/recipes/RecipesFragment.kt
+++ app/src/main/java/dev/haziqkamel/foody/ui/fragments/recipes/RecipesFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
 import dev.haziqkamel.foody.viewmodels.MainViewModel
@@ -48,6 +49,10 @@ class RecipesFragment : Fragment() {
 
         setupRecyclerView()
         readDatabase()
+
+        binding.recipesFab.setOnClickListener{
+            findNavController().navigate(R.id.action_recipesFragment_to_recipesBottomSheet)
+        }
 
         return binding.root
     }
