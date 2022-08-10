4a72848da8b81745e12f06709e85a625afa8f1c0
haziqfireworks <109516759+haziqfireworks@users.noreply.github.com>
2022-08-10 10:31:48 +0800

    Modify RecipesDiffUtil class to accept Generic

--- app/src/main/java/dev/haziqkamel/foody/util/RecipesDiffUtil.kt
+++ app/src/main/java/dev/haziqkamel/foody/util/RecipesDiffUtil.kt
@@ -3,9 +3,9 @@ package dev.haziqkamel.foody.util
 import androidx.recyclerview.widget.DiffUtil
 import dev.haziqkamel.foody.models.foodRecipe.Result
 
-class RecipesDiffUtil(
-    private val oldList: List<Result>,
-    private val newList: List<Result>
+class RecipesDiffUtil<T>(
+    private val oldList: List<T>,
+    private val newList: List<T>
 ) : DiffUtil.Callback() {
 
     override fun getOldListSize(): Int {
