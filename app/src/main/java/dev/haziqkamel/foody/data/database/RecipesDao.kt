21da41232b1fa6e631b0dcb8e77737baddbd83ca
haziqfireworks <109516759+haziqfireworks@users.noreply.github.com>
2022-08-10 14:44:16 +0800

    Modify RecipesDao Interface

--- app/src/main/java/dev/haziqkamel/foody/data/database/RecipesDao.kt
+++ app/src/main/java/dev/haziqkamel/foody/data/database/RecipesDao.kt
@@ -1,9 +1,7 @@
 package dev.haziqkamel.foody.data.database
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
+import dev.haziqkamel.foody.data.database.entities.FavoritesEntity
 import dev.haziqkamel.foody.data.database.entities.RecipesEntity
 import kotlinx.coroutines.flow.Flow
 
@@ -13,7 +11,19 @@ interface RecipesDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertRecipes(recipesEntity: RecipesEntity)
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertFavoriteRecipe(favoritesEntity: FavoritesEntity)
+
     @Query("SELECT * FROM recipes_tb ORDER BY id ASC")
     fun readRecipes(): Flow<List<RecipesEntity>>
+
+    @Query("SELECT * FROM favorite_recipe_table ORDER BY id ASC")
+    fun readFavoriteRecipes(): Flow<List<FavoritesEntity>>
+
+    @Delete
+    suspend fun deleteFavoriteRecipes(favoritesEntity: FavoritesEntity)
+
+    @Query("DELETE FROM favorite_recipe_table")
+    suspend fun deleteAllFavoriteRecipes()
 
 }
