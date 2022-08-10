8b4997eca62b002688acab0d8f88d5ea632c0d61
haziqfireworks <109516759+haziqfireworks@users.noreply.github.com>
2022-08-10 14:50:32 +0800

    Modify LocalDataSource class

--- app/src/main/java/dev/haziqkamel/foody/viewmodels/MainViewModel.kt
+++ app/src/main/java/dev/haziqkamel/foody/viewmodels/MainViewModel.kt
@@ -25,7 +25,7 @@ class MainViewModel @Inject constructor(
 
     /** ROOM DATABASE */
 
-    val readRecipes: LiveData<List<RecipesEntity>> = repository.local.readDatabase().asLiveData()
+    val readRecipes: LiveData<List<RecipesEntity>> = repository.local.readRecipes().asLiveData()
 
     private fun insertRecipes(recipesEntity: RecipesEntity) =
         viewModelScope.launch(Dispatchers.IO) {
