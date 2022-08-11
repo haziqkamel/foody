a4e9c56973d299b14a32bfacaf479401ca4c185c
haziqfireworks <109516759+haziqfireworks@users.noreply.github.com>
2022-08-11 16:30:28 +0800

    Apply Contextual Action Mode Style

--- app/src/main/java/dev/haziqkamel/foody/adapters/FavoriteRecipeAdapter.kt
+++ app/src/main/java/dev/haziqkamel/foody/adapters/FavoriteRecipeAdapter.kt
@@ -1,6 +1,7 @@
 package dev.haziqkamel.foody.adapters
 
 import android.view.*
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentActivity
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.DiffUtil
@@ -79,6 +80,7 @@ class FavoriteRecipeAdapter(
 
     override fun onCreateActionMode(mode: ActionMode?, menu: Menu?): Boolean {
         mode?.menuInflater?.inflate(R.menu.favorite_contextual_menu, menu)
+        applyStatusBarColor(R.color.contextualStatusBarColor)
         return true
     }
 
@@ -91,5 +93,10 @@ class FavoriteRecipeAdapter(
     }
 
     override fun onDestroyActionMode(mode: ActionMode?) {
+        applyStatusBarColor(R.color.statusBarColor)
+    }
+
+    private fun applyStatusBarColor(color: Int) {
+        requireActivity.window.statusBarColor = ContextCompat.getColor(requireActivity, color)
     }
 }
