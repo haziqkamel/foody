73835c65e97ab7443d53f7e7063da39c377ed55f
haziqfireworks <109516759+haziqfireworks@users.noreply.github.com>
2022-08-11 14:52:28 +0800

    Display Favorite Recipe

--- app/src/main/java/dev/haziqkamel/foody/ui/fragments/favoriteRecipes/FavoriteRecipesFragment.kt
+++ app/src/main/java/dev/haziqkamel/foody/ui/fragments/favoriteRecipes/FavoriteRecipesFragment.kt
@@ -5,17 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import dagger.hilt.android.AndroidEntryPoint
 import dev.haziqkamel.foody.R
+import dev.haziqkamel.foody.adapters.FavoriteRecipeAdapter
+import dev.haziqkamel.foody.viewmodels.MainViewModel
+import kotlinx.android.synthetic.main.fragment_favorite_recipes.view.*
 
-
+@AndroidEntryPoint
 class FavoriteRecipesFragment : Fragment() {
+
+    private val mAdapter: FavoriteRecipeAdapter by lazy { FavoriteRecipeAdapter() }
+    private val mainViewModel: MainViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite_recipes, container, false)
+        val view = inflater.inflate(R.layout.fragment_favorite_recipes, container, false)
+
+        setupRecyclerView(view.favorite_recipe_rv)
+
+        mainViewModel.readFavoriteRecipes.observe(viewLifecycleOwner) { favoriteEntity ->
+            mAdapter.setData(favoriteEntity)
+        }
+
+        return view
     }
 
+    private fun setupRecyclerView(recyclerView: RecyclerView) {
+        recyclerView.adapter = mAdapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+    }
 }
